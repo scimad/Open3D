f4cb4ec9ee80b02e028675bc1b0c1fead2b68fb3
scimad <scimad2.71@gmail.com>
2020-03-01 22:51:46 +0545

    Read pinhole model (from static filepath for now)

--- examples/Python/Advanced/realtime-kinfu.py
+++ examples/Python/Advanced/realtime-kinfu.py
@@ -2,43 +2,48 @@ import argparse
 import open3d as o3d
 
 class FrameGen(object):
-        def __init__(self, config, source):
-            self.n_frames = 0
-            self.source = source
-            self.sensor = o3d.io.AzureKinectSensor(config)
-            self.flag_completed = False
+    def __init__(self, config, source):
+        self.n_frames = 0
+        self.source = source
+        self.sensor = o3d.io.AzureKinectSensor(config)
+        self.flag_completed = False
 
-            if self.source == 'kinect':
-                if not self.sensor.connect(0):
-                    raise RuntimeError('Failed to connect to sensor')
-            else:
-                video_path = self.source
-                print ("I am going to read rgbd frames from this video")
-                self.reader = o3d.io.AzureKinectMKVReader()
-                self.reader.open(video_path)
-                if not self.reader.is_opened():
-                    raise RuntimeError("Not a valid source: {}".format(video_path))
+        if self.source == 'kinect':
+            if not self.sensor.connect(0):
+                raise RuntimeError('Failed to connect to sensor')
+        else:
+            video_path = self.source
+            print ("I am going to read rgbd frames from this video")
+            self.reader = o3d.io.AzureKinectMKVReader()
+            self.reader.open(video_path)
+            self.metadata = self.reader.get_metadata()
+            if not self.reader.is_opened():
+                raise RuntimeError("Not a valid source: {}".format(video_path))
 
-        def get_next_frame(self):
-            if self.source == 'kinect':
-                print ("lolo")
-                while not self.flag_completed:
-                    rgbd = self.sensor.capture_frame(align_depth_to_color=True)
-                    if rgbd is None:
-                        continue
-                    print ("New frame is read from sensor!")
-                    self.n_frames += 1
-                    yield rgbd, self.n_frames
-            else:
-                while not self.reader.is_eof() and not self.flag_completed:
-                    rgbd = self.reader.next_frame()
-                    if rgbd is None:
-                        continue
-                    print ("New frame is read from file!")
-                    self.n_frames += 1
-                    print ("------------------- rgbd:", rgbd)
-                    yield (rgbd)
-                self.reader.close()
+    def get_next_frame(self):
+        if self.source == 'kinect':
+            print ("lolo")
+            while not self.flag_completed:
+                rgbd = self.sensor.capture_frame(align_depth_to_color=True)
+                if rgbd is None:
+                    continue
+                print ("New frame is read from sensor!")
+                self.n_frames += 1
+                yield rgbd, self.n_frames
+        else:
+            while not self.reader.is_eof() and not self.flag_completed:
+                rgbd = self.reader.next_frame()
+                if rgbd is None:
+                    continue
+                print ("New frame is read from file!")
+                self.n_frames += 1
+                print ("------------------- rgbd:", rgbd)
+                yield (rgbd)
+            self.reader.close()
+
+class KinectFusionConfig(object):
+    def __init__(self):
+        self.camera_intrinsic = o3d.io.read_pinhole_camera_intrinsic('/home/scimad/EK/Work/VR360/Open3D/examples/Python/Advanced/scratch/intrinsic_config.json')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Kinect Fusion')
@@ -52,12 +57,10 @@ if __name__ == "__main__":
 
     frame_gen = FrameGen(config, args.source)
 
+    my_kinfu_config = KinectFusionConfig()
     
-
     #TODO: Retrieve Calibrarion: https://github.com/microsoft/Azure-Kinect-Samples/blob/edb6c364eb7fb86638327c7a1b3da1833b85d9a0/opencv-kinfu-samples/main.cpp#L432
     #TODO: Start Cameras
-    #TODO: Generate a pinhole model for depth camera
-    #TODO: Retrieve calibration parameters (and width and height)
     #TODO: Initialize Kinfu parameters
     #TODO: Initialize Distortion coefficients
     #TODO: Create KinectFusion Module Instance
